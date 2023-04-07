a5c2e78cca4908cae2f9182255d4c4eeddb1c56a
Naiem Islam <naiem4288@gmail.com>
2023-04-07 07:54:33 +0600

    implemented reaction feature on post

--- src/api/reaction/controllers/reaction.js
+++ src/api/reaction/controllers/reaction.js
@@ -1,9 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  * reaction controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::reaction.reaction');
+module.exports = createCoreController("api::reaction.reaction", (strapi) => ({
+  async reacts(ctx) {
+    try {
+      const user = ctx.state.user;
+      ctx.request.body.data.user = user.id;
+      ctx.request.body.data.post = ctx.params.id;
+
+      console.log("user>>", ctx.request.body.data.user);
+      console.log("post>>", ctx.request.body.data.post);
+      const response = await super.create(ctx);
+      console.log(response);
+
+      return response;
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+}));
