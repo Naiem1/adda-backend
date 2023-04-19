ed1a2031acaf53b60c00fdefcd76b2b1edbba78a
Naiem Islam <naiem4288@gmail.com>
2023-04-19 12:36:19 +0600

    added environment variables

--- config/database.js
+++ config/database.js
@@ -1,16 +1,17 @@
-const path = require('path');
+const path = require("path");
 
-    // strapi-api/config/database.js
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'adda'),
-          user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', 'admin'),
-        },
-        debug: false,
-      },
-    });
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA"),
+    },
+    debug: false,
+  },
+});
