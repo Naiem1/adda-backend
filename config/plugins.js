7697237dacea66708dda9a7f552d7107db81f3fd
Naiem Islam <naiem4288@gmail.com>
2023-04-19 20:42:00 +0600

    add some code

--- config/plugins.js
+++ config/plugins.js
@@ -14,3 +14,19 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+module.exports = {
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
+  },
+};
